58414c54e44ea35c99427ffa2eb2d8287c70c796
kenobi82403 <kenobi82403@gmail.com>
2016-11-19 02:16:07 -0500

    modified vampires.rb to include detection logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,13 +14,27 @@ employee_age = gets.chomp
 puts "What year were you born?"
 year_of_birth = gets.chomp
 
+  # Test if vampire age match
+  calculate_age = Time.new.to_s[0..3].to_i - year_of_birth.to_i
+  age_match = (employee_age.to_i === calculate_age)
+
 # Our company cafeteria serves garlic bread. Should we order some for you?
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-order_garlic = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+order_garlic = gets.chomp.downcase
+if order_garlic === "yes"
+  order_garlic = true
+elsif order_garlic === "no"
+  order_garlic = false 
+end
 
 # Would you like to enroll in the company's health insurance?
-puts "Would you like to enroll in the company's health insurance?"
-need_insurance = gets.chomp
+puts "Would you like to enroll in the company's health insurance? (yes/no)"
+need_insurance = gets.chomp.downcase
+if need_insurance === "yes"
+  need_insurance = true
+elsif need_insurance === "no"
+  need_insurance = false 
+end
 
 puts
 puts "Employee Name: #{employee_name}"
@@ -28,3 +42,33 @@ puts "Employee Age: #{employee_age}"
 puts "Year of Birth: #{year_of_birth}"
 puts "Order Garlic? #{order_garlic}"
 puts "Insurance? #{need_insurance}"
+
+# Release 2 Detection Logic
+
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+if employee_name === "Drake Cula" || employee_name === "Tu Fang"
+  result = "Definitely a vampire"
+
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+elsif age_match === false && 
+        order_garlic === false && 
+          need_insurance === false
+  result = "Almost certainly a vampire"
+
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+elsif age_match === false && 
+        (order_garlic === false || need_insurance === false)
+  result = "Probably a vampire"
+
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+elsif age_match && 
+    (order_garlic || need_insurance)
+  result = "Probably not a vampire"
+
+# Otherwise, print “Results inconclusive.”
+else
+  result = "Results inconclusive"
+end
+
+puts
+puts "Result: #{result}"
