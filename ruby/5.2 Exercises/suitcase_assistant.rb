7ce74e89148a9e3ceab9b3ecc6841aa7864b8443
[KENDY NG] <kenobi82403@gmail.com>
2016-11-24 03:58:55 +0000

    modified suitcase_assistant.rb

--- ruby/5.2 Exercises/suitcase_assistant.rb
+++ ruby/5.2 Exercises/suitcase_assistant.rb
@@ -1,36 +1,47 @@
+suitcase_items = []
+
 puts "Welcome to the Suitcase Assistant!"
 
-puts "Enter an item:"
-item_1 = gets.chomp
-
-puts "Enter an item:"
-item_2 = gets.chomp
-
-puts "Enter an item:"
-item_3 = gets.chomp
+loop do
+  puts "Enter an item (or type 'exit'):"
+  input = gets.chomp
+  break if input == 'exit'
+  suitcase_items << input
+end
 
 puts "Here's your packing list:"
-puts item_1
-puts item_2 
-puts item_3
-
+count = 1
+suitcase_items.each do |items|
+  puts count.to_s + ": " + items
+  count += 1
+end
+puts 
 puts "Safe Travels!"
 
-numbers = []
 
-numbers << 4
+# ==================================
+
+# some array methods
+# arr.length
+# arr.first
+# arr.last
+# arr[-1] 
+
+# numbers = []
+
+# numbers << 4
 # => [4]
 
-numbers << 5
+# numbers << 5
 # => [4, 5]
 
-numbers << 8
+# numbers << 8
 # => [4, 5, 8]
 
-numbers << 8
+# numbers << 8
 # => [4, 5, 8, 8]
 
-numbers.delete(8) # Delete all instances of 8
+# numbers.delete(8) # Delete all instances of 8
 # => 8 
 # => [4, 5]
 # or 
