714fa479f263b912bada98751c1b0b1295836dc6
kenobi82403 <kenobi82403@gmail.com>
2016-11-23 20:32:26 -0500

    modified iteration.rb file and create own method with bloc

---
+++ ruby/iteration.rb
@@ -0,0 +1,11 @@
+# Release 0
+
+# def fancy_message 
+#   puts "This is before the awesome message"
+#   yield("Yibin", "Kendy")
+#   puts "this is after the awesome message"
+# end
+
+# fancy_message {|name1, name2| puts "#{name1} is pairing with #{name2}!"}
+
+# 
