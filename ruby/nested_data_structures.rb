2e221df261f5e7c896cfb5cd075a5414d177e211
kenobi82403 <kenobi82403@gmail.com>
2016-11-25 23:12:17 -0500

    created hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+# highway construct
+#   car => has people => name, age, clothes, occupation
+
+highway = {
+  "car 1" = {
+    mom: {
+      name: "Mary Pooh",
+      age: 40,
+      hobbies: [
+        'knit',
+        'music',
+        'football'
+      ]
+      occupation: [
+        'CEO',
+        'Mary Sue Corporation'
+      ]
+    }
+    dad: {
+      name: "John Pooh",
+      age: 41,
+      hobbies: [
+        'video games',
+        'music',
+        'woodwork'
+      ]
+      occupation: [
+        'Creative Director',
+        'Mary Sue Corporation'
+      ]
+    }
+    son: {
+      name: "Sam Pooh",
+      age: 18,
+      hobbies: [
+        'video games',
+        'music',
+        'football'
+      ]
+      occupation: [
+        'student',
+        'Dev Bootcamp'
+      ]
+    }
+  }
+}
+
+# What is mom's occupation?
+highway['car 1'][:mom][:occupation][0] # => CEO
