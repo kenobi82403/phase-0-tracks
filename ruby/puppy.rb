47886e93728eef7ab6e74d5f96273d8777acd0f9
[KENDY NG] <kenobi82403@gmail.com>
2016-11-30 18:32:37 +0000

    designed puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# Puppy ------------------------
+# Chihuahua
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Name: varies
+# Color: varies
+# Fur_length: short
+# Size: small
+# Max_life: 18
+
+# BEHAVIOR -----------------------
+# Jump
+# Sit
+# Fetch
+# Bark
+# Whine
