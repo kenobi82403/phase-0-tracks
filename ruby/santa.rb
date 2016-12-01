a435b4193d04c3958cf2e21bd5ca801bd2ab4a41
[KENDY NG] <kenobi82403@gmail.com>
2016-12-01 19:52:22 +0000

    created and tested speak and eat milk and chookies method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,8 +15,17 @@ class Santa
     puts 'Initializing Santa Instance...'
   end
 
+  def speak
+    puts "Ho, ho, ho! Haaappy holidays!"
+  end
+  
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
 
 end
 
 # Create new Santa instance and call it Nick
 Nick = Santa.new
+Nick.speak
+Nick.eat_milk_and_cookies("Oatmeal")
