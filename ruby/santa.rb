c4d81d2f24d7b9f48512fd46d3cef9c78923ac67
[KENDY NG] <kenobi82403@gmail.com>
2016-12-01 21:22:09 +0000

    Release 1: Give Santa Attributes for Christmas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,23 +1,14 @@
-# Santa................
-
-# Characteristic.......
-
-
-# Behavior.............
 # speak = 'Ho, ho, ho! Haaappy holidays!"
 # eat milk and cookies = takes a cookie type and print "That was a good <type of cookie here>!"
 
-# gender, which will be a string passed in on initialization
-# ethnicity, which will be a string passed in on initialization
-# reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-# age, which is not passed in on initialization and defaults to 0
-
 class Santa
   # initialize method that prints "Initializing Santa Instance..."
-  def initialize(gender, ethnicity)
+  def initialize(name, gender, ethnicity, time_period)
     puts 'Initializing Santa Instance...'
+    @name = name
     @gender = gender
     @ethnicity = ethnicity
+    @time_period = time_period
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
@@ -33,6 +24,118 @@ class Santa
 end
 
 # Create new Santa instance and call it Nick
-Nick = Santa.new
-Nick.speak
-Nick.eat_milk_and_cookies("Oatmeal")
+# Nick = Santa.new
+# Nick.speak
+# Nick.eat_milk_and_cookies("Oatmeal")
+
+santas = []
+
+# Adding Santa instances in Array
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# Refactored Santa Instances in Array
+santa_genders = [
+  "agender", 
+  "female", 
+  "bigender", 
+  "male", 
+  "female", 
+  "gender fluid", 
+  "N/A"
+]
+
+santa_ethnicities = [
+  "black", 
+  "Latino", 
+  "white", 
+  "Japanese-African", 
+  "prefer not to say", 
+  "Mystical Creature(unicorn)", 
+  "N/A"
+]
+  
+santa_name = [
+  "Harley Claesson",
+  "Zlatica Frank",
+  "Eusebius Langenberg",
+  "Xanthos Duffy",
+  "Clara Sokal",
+  "Lucilius Mathilda",
+  "Jolanda Kroger"
+]
+  
+santa_time_period = [
+  "Ice age",
+  "Middle age",
+  "Machine age",
+  "Renaissance",
+  "Golden age",
+  "Dark age",
+  "Modern"
+]
+  
+  
+length = santa_name.length
+
+length.times do |i|
+  santas << Santa.new(
+              santa_genders[i], 
+              santa_ethnicities[i],
+              santa_name[i],
+              santa_time_period
+            )
+end
+
+length.times do |i|
+  p santas[i]
+end
+
+# Test each instance of array with instance method
+santas.each { |int| puts int.speak }
+
+
+
+
+
+
+
+
+
+
+
+################################ Notes
+
+# class Puppy
+#   def initialize(name)
+#   	@name = name
+#   end
+	
+#   def bark
+#   	puts "#{@name} says: Woof!"
+#   end
+# end
+
+# # we make an array of names to build puppies with
+# names = ["Fido", "Spot", "Duchess", "Ginger"]
+
+# # we make an empty container for our puppy collection
+# puppies = []
+
+# puts "Iterating through names list to create puppies ..."
+# names.each do |name|
+#   puts "Creating a puppy named #{name} ..."
+#   puppies << Puppy.new(name)
+#   puts "There are now #{puppies.length} Puppy instances in the array"
+#   puts "----"
+# end
+
+# puts "Testing each Puppy instance in the array to make sure it can bark ..."
+# puppies.each do |puppy|
+#   puppy.bark
+# end
