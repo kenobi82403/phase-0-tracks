d7203fff38289da4eedc19ffad11bdac9e8127cd
[KENDY NG] <kenobi82403@gmail.com>
2016-12-01 20:44:16 +0000

    added in attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,12 +7,19 @@
 # speak = 'Ho, ho, ho! Haaappy holidays!"
 # eat milk and cookies = takes a cookie type and print "That was a good <type of cookie here>!"
 
-
+# gender, which will be a string passed in on initialization
+# ethnicity, which will be a string passed in on initialization
+# reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+# age, which is not passed in on initialization and defaults to 0
 
 class Santa
   # initialize method that prints "Initializing Santa Instance..."
-  def initialize
+  def initialize(gender, ethnicity)
     puts 'Initializing Santa Instance...'
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
