00082f8cf8afd5d38057ba7c484977f024d8be2b
[KENDY NG] <kenobi82403@gmail.com>
2016-12-01 19:08:56 +0000

    created new Santa class, and a working initialize method

---
+++ ruby/santa.rb
@@ -0,0 +1,22 @@
+# Santa................
+
+# Characteristic.......
+
+
+# Behavior.............
+# speak = 'Ho, ho, ho! Haaappy holidays!"
+# eat milk and cookies = takes a cookie type and print "That was a good <type of cookie here>!"
+
+
+
+class Santa
+  # initialize method that prints "Initializing Santa Instance..."
+  def initialize
+    puts 'Initializing Santa Instance...'
+  end
+
+
+end
+
+# Create new Santa instance and call it Nick
+Nick = Santa.new
