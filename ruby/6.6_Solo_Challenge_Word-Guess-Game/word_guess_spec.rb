3cc70e19541cd1bb923d9442af7b248e7755e227
kenobi82403 <kenobi82403@gmail.com>
2016-12-04 17:01:08 -0500

    added new rspec tests and fixed 'require' function

--- ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess_spec.rb
+++ ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess_spec.rb
@@ -9,23 +9,55 @@
 # The user should get a congratulatory message if they win, and a taunting message if they lose.
 
 # load word_guess.rb
-require 'word_guess'
+require_relative 'word_guess'
 
 # describe block for testing WordGuessGame class
 describe WordGuessGame do
   # Initiate a WordGuessGame class for testing
   
-  
-  # Guesses are limited to the length of the secret word
+  # Allow one user to input a word string as the secret word and save that word into the instance variable
+  it "should return the string as the value of the attribute @secret_word entered by the user" do
 
+  end
+
+  # Allow guesser to input letter string as guesses and it will return a string showing the current state of the blank word puzzle
+  it "should return a letter string showing the current state of the blank word puzzle every time the user guesses" do
+
+  end
+
+  # Keep track of each guess by the user and save it into an array
+  it "push the letter string into an array when user enter a new input" do 
+
+  end
+
+  # Increase the guess counter every time the user guess wrong
+  it "doesn't push the letter string into an array if user enter a repeated input" do 
+
+  end
+
+  # Increase the guess counter every time the user guess wrong
+  it "increases the guess counter, when user enter a new incorrect input" do 
+
+  end
 
   # Repeated guesses does not count against remaining guesses
+  it "doesn't increase the guess counter when user enter a repeated input" do 
 
+  end
+
+  # Guesses are limited to the length of the secret word
+  it "stops the game when the user used up its guess allowance" do 
+
+  end
 
   # User get a congratulatory message if they win
+  it "returns a string when the user wins" do 
 
+  end
 
   # User get a taunting message if they lose
+  it "returns a string when the user loses" do
 
+  end
 
 end
