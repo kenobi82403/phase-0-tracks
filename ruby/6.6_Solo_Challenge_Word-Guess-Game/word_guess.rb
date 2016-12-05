23f1f45c04ff73c294d738ca8fee335249e71ec3
kenobi82403 <kenobi82403@gmail.com>
2016-12-05 03:03:01 -0500

    added userinterface, cleaned up code, and fixed rspec tests

--- ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess.rb
+++ ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess.rb
@@ -1,10 +1,9 @@
-# DESIGN
-
+# GAME LOGIC
   # class for WordGuessGame
 class WordGuessGame
-  # Getter method on instance variable secret_word
+  # Getter methods
   attr_reader :secret_word, 
-              :guess_allowed,
+              :wrong_guess_allowed,
               :guess_count,
               :guess_history, 
               :is_over, 
@@ -13,87 +12,96 @@ class WordGuessGame
   # Instance method to initialize
   #     input: a string
   #     steps: 
-  #         1). Instance variable 'secret_word' equal to the string input removing all white spaces front and end of the word
-  #         2). Instance variable 'guess_remaining' equal to length of the secret_word by calling the length method on the string
-  #         2). Instance variable 'guess_history' equal to length of the secret_word by calling the length method on the string
-  #         3). Instance variable 'is_over' equal to false
-  #         4). Instance variable 'word puzzle' equal to underscores equal to the length of the word will need to turn secret word to array and call a join method on it to separate by spaces
-  #     output:  a string
+  #         convert secret word to down case and strip all white spaces
+  #         Instance variable to store secret word string
+  #         Instance variable to store wrong guess allowance up to length of secret word, integer
+  #         Instance variable to set guess count to zero, an integer
+  #         Instance variable to keep track of guess history, an empty array
+  #         Instance variable to see if game is over, accepts a boolean
+  #         Instance variable to convert secret word to blank spaces, convert from string to array
+  #     output:  new instance class with attributes
   def initialize(secret_word)
+    secret_word = secret_word.downcase.strip
     @secret_word = secret_word
-    @guess_allowed = secret_word.length
+    @wrong_guess_allowed = secret_word.length
     @guess_count = 0
     @guess_history = []
     @is_over = false
     @puzzle = ('_' * secret_word.length).split('')
-    print_puzzle
   end
 
   # Instance method to guess letter 
   #     input: a letter string
   #     steps: 
-  #         1). IF letter was not guessed 
-  #                 - enter the letter to the 'guess history' instance variable
+  #         1). IF letter is not in the guess history array and not in the secret word
   #                 - increase guess_count by 1
+  #                 - enter the letter to the 'guess history' instance variable
   #         2). Iterate over the secret word using string#times method
   #                 - convert secret word to an array by using a split method
   #                 - check if each array value equals to the letter, if so, use that same index number to update the word puzzle result
-  #         3). check IF 'word puzzle' equals to 'secret word' 
-  #                 'Great job! You've won'
-  #                 if not, check IF we still have any more guesses? If no more 
-  #                     set is_over to true
-  #       call print puzzle
-  #     output:  a string
+  #         3). IF 'word puzzle' equals to 'secret word' or if guess_count equal to or exceed guess allowance
+  #                 - game is over
+  #         4). return is over
+  #     output:  a boolean
   def guess_letter(letter)
-
-    if !guess_history.index(letter)
+    letter = letter.downcase.strip
+    abc = 'abcdefghijklmnopqrstuvwxyz'
+    if letter.length > 1 || !abc.index(letter)
+      return false
+    elsif !guess_history.index(letter) && !@secret_word.index(letter)
       @guess_count += 1
       @guess_history << letter
-      @secret_word.length.times do |index|
-        if @secret_word.split('')[index] == letter
+    end
+
+    @secret_word.length.times do |index|
+      if @secret_word.split('')[index] == letter
           @puzzle[index] = letter
-       end
       end
     end
 
-    check_guess_count
-    print_puzzle
-  end
-
-  # Instance method to check guess count
-  def check_guess_count
-    if @guess_count >= 5
+    if @puzzle.join('') == @secret_word || @guess_count >= @wrong_guess_allowed
       @is_over = true
-      "Sorry, you have used up your guess allowance! :( \n Better luck next time!"
-    end
-  end
-
-  # Instance method to check if won
-  def check_if_won
-    if @puzzle.join('') == @secret_word
-      "Woohoo! You've won! Good job playa!"
     end
   end
 
   # Instance method to print current state
   # input: None
-  # steps: 
-  #     1). divider to set it apart from other outputs
-  #     2). puts on the current state of the word puzzle
-  # output:  a string
+  # steps: converts puzzle array to string
+  # output:  puzzle string
   def print_puzzle
     @puzzle.join(' ')
   end
 end
 
 # USER INTERFACE / DRIVER CODES
-# game = WordGuessGame.new('kendy')
-# game.guess_letter('a')
-# game.guess_letter('b')
-# game.guess_letter('c')
-# game.guess_letter('d')
-# game.guess_letter('e')
-# game.guess_letter('f')
-# p game.guess_history
-# p game.guess_count
-# puts game.print_puzzle == '_ e _ d _'
+puts "Hey! Great to have you here. Are you ready to play the greatest word guess game!"
+puts
+puts "Okay.. Player two please don't peek!"
+puts 
+puts "Player one, please input your secret word.."
+game = WordGuessGame.new(gets.chomp) # get secret word from player one
+
+until game.is_over
+  puts
+  puts "Player two, please guess one letter?"
+  game.guess_letter(gets.chomp)
+
+  # Output the progress of the game
+  puts
+  puts game.print_puzzle
+  puts
+  puts "You have #{game.wrong_guess_allowed - game.guess_count} wrong guess allowance.."
+  puts
+
+  if game.guess_count >= game.wrong_guess_allowed
+    puts "Sorry, you have used up your guess allowance!"
+    puts "The secret word was '#{game.secret_word}'"
+    puts "Better luck next time!"
+  end
+  
+  if game.puzzle.join('') == game.secret_word
+    puts "Woohoo! You've won! Good job playa!"
+  end
+end
+puts
+puts "Thank you for playing the greatest Word Guess Game! Please come again!"
--- ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess_spec.rb
+++ ruby/6.6_Solo_Challenge_Word-Guess-Game/word_guess_spec.rb
@@ -1,26 +1,16 @@
-# One user can enter a word (or phrase, if you would like your game to support that), and another user attempts to guess the word.
-
-# Guesses are limited, and the number of guesses available is related to the length of the word.
-
-# Repeated guesses do not count against the user.
-
-# The guessing player receives continual feedback on the current state of the word. So if the secret word is "unicorn", the user will start out seeing something like "_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
-
-# The user should get a congratulatory message if they win, and a taunting message if they lose.
-
 # load word_guess.rb
 require_relative 'word_guess'
 
 # describe block for testing WordGuessGame class
 describe WordGuessGame do
   # Initiate a WordGuessGame class for testing
-  let(:game){WordGuessGame.new('kendy')}
+  let(:game){WordGuessGame.new('    kendY ')}
   
   # Test initiate method to have set attributes secret_word, guess_remaining, guess_history, is_over, and puzzle
   it "should initiate and set the following attributes: secret_word, guess_remaining, is_over, and puzzle" do
     expect(game).to have_attributes(
       :secret_word => 'kendy',
-      :guess_allowed => 'kendy'.length,
+      :wrong_guess_allowed => 'kendy'.length,
       :guess_count => 0,
       :guess_history => [],
       :is_over => false,
@@ -33,14 +23,24 @@ describe WordGuessGame do
     game.guess_letter('e')
     game.guess_letter('e')  
     game.guess_letter('d')
-    expect(game.guess_history).to eq ['e','d']
+    game.guess_letter('w')
+    expect(game.guess_history).to eq ['w']
   end
 
-  # Increase the guess counter every time the user enter an input but does not increase when guesses were repeated
-  it "increases the guess counter, when user enter a new input" do 
+  # Keep track of each guess by the user and save it into an array and does not allow repeated letters
+  it "push the letter string into an array when user enter a new input" do 
     game.guess_letter('e')
     game.guess_letter('e')  
     game.guess_letter('d')
+    expect(game.guess_history).to eq []
+  end
+
+  # Increase the guess counter every time the user enter an input but does not increase on repeated guess or guess is correct
+  it "increases the guess counter, when user enter a new input" do 
+    game.guess_letter('Ee')
+    game.guess_letter('e')  
+    game.guess_letter('d')
+    game.guess_letter('w')
     game.guess_letter('w')
     expect(game.guess_count).to eq 1
   end
@@ -51,15 +51,6 @@ describe WordGuessGame do
     expect(game.print_puzzle).to eq '_ e _ _ _'
   end
 
-  # test to make sure no two letter string are entered into the array
-  # it "doesn't push the letter string into an array if user enter a repeated input" do 
-  # end
-
-
-  # Repeated guesses does not count against remaining guesses
-  # it "doesn't increase the guess counter when user enter a repeated input" do 
-  # end
-
   # Guesses are limited to the length of the secret word
   it "stops the game when the user used up its guess allowance" do 
     game.guess_letter('a')
@@ -68,28 +59,17 @@ describe WordGuessGame do
     game.guess_letter('d')
     game.guess_letter('e')
     game.guess_letter('f')
+    game.guess_letter('g')
     expect(game.is_over).to eq true
   end
 
-  # User get a congratulatory message if they win
-  it "returns a string when the user wins" do 
+  # Stops the game when user guess all the correct letters
+  it "Stops the game when user guess all the correct letters" do 
     game.guess_letter('k')
     game.guess_letter('e')
     game.guess_letter('n')
     game.guess_letter('d')
     game.guess_letter('y')
-    expect(game.check_if_won).to eq "Woohoo! You've won! Good job playa!"
+    expect(game.is_over).to eq true
   end
-
-  # User get a taunting message if they lose
-  it "returns a string when the user loses" do
-    game.guess_letter('a')
-    game.guess_letter('b')
-    game.guess_letter('c')
-    game.guess_letter('d')
-    game.guess_letter('e')
-    game.guess_letter('f')
-    expect(game.check_guess_count).to eq "Sorry, you have used up your guess allowance! :( \n Better luck next time!"
-  end
-
 end
