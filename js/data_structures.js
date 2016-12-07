21a82f29cf5c68fcb2be4add7603b264a57580ca
[KENDY NG] <kenobi82403@gmail.com>
2016-12-07 00:56:00 +0000

    created constructor for Car, release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,36 @@ for(var i = 0; i < fourColors.length; i++) {
 };
 
 console.log(horses);
+
+// Release 2
+
+function Cars(brand, type, color, speed) {
+  this.brand = brand;
+  this.type = type;
+  this.color = color;
+  this.maxSpeed = speed;
+  this.acceleration = function() {
+    if(this.maxSpeed > 100) {
+      console.log('Keep going!');
+    } else {
+      console.log('Need more power!');
+    }
+  };
+}
+
+var bmw = new Cars('bmw', 'xi', 'beige', '101');
+console.log(
+  bmw.brand,
+  bmw.type,
+  bmw.color,
+  bmw.maxSpeed
+  );
+
+  bmw.acceleration();
+
+
+
+
+
+
+
