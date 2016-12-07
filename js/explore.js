829e98c7a8e36a928ea85081c81508be97a09e22
kenobi82403 <kenobi82403@gmail.com>
2016-12-07 03:09:56 -0500

    added driver code, and fixed for loop starting index

--- js/explore.js
+++ js/explore.js
@@ -2,3 +2,15 @@
 // - create an empty string var
 // - iterate over new string backwards and concat each letter to the new string var
 // - return the new string var
+function reverse(string) {
+  var result = "";
+  for(var i = string.length-1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
+}
+
+// Driver Code
+
+console.log(reverse("Hello")); // olleH
+console.log(reverse("Kendy")); // ydenK
