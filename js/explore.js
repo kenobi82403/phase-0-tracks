e7536bd8535f80de9be0bf0e997bb846873f973f
kenobi82403 <kenobi82403@gmail.com>
2016-12-07 03:04:48 -0500

    pseudo code reverse method

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// Reverse Function - take's in a string
+// - create an empty string var
+// - iterate over new string backwards and concat each letter to the new string var
+// - return the new string var
