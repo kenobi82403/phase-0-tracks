805cb2c5e1fa7ff223959f8b471a9dff524f1138
[KENDY NG] <kenobi82403@gmail.com>
2016-12-07 00:36:36 +0000

    created horses object for release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,13 @@ var fourNames = ["Ed", "Nanu", "Max", "Ken"];
 
 fourColors.push("black");
 fourNames.push("Ollie");
+
+// Release 1
+
+var horses = {};
+
+for(var i = 0; i < fourColors.length; i++) {
+  horses[fourNames[i]] = fourColors[i];
+};
+
+console.log(horses);
