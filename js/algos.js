1c3cf0cad5483b16d4f63bd553d780f1bb3e3677
kenobi82403 <kenobi82403@gmail.com>
2016-12-07 21:02:30 -0500

    added longestWord algorithm

--- js/algos.js
+++ js/algos.js
@@ -1,15 +1,28 @@
 // Release 0 : Find the Longest Phrase
 
-// In algos.js, write a function that takes an array of words or phrases and returns the longest word or phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason through this based on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. Note that "some string".length will do what you expect in JS.
 // Algorithm
 
 // Function takes in Array of words/phrase
-//  - a variable to keep track of the index of the longest word with initial value of 0
-//  - iterate the whole Array
+//  - a variable to keep track of the longest word
+//  - a variable keep track of the length of the longest word
+//  - iterate the whole Array using a for loop
 //  - condition to see if the current word length of the index is larger than maxIndex
-//  - if so, then override with the current word length
+//  - if so, then save the length of the word and the resulting word in their respective variable
 // Returns a string by referencing the array with the maxIndex variable
-
-
+function longestWord(words_array){
+  var result = "";
+  var maxLength = 0;
+  for(var i = 0; i < words_array.length; i++){
+    if (words_array[i].length > maxLength){
+      maxLength = words_array[i].length;
+      result = words_array[i];
+    }
+  }
+  return result;
+}
 
 // DRIVER CODE
+var testArray = ["Driver", "Duck", "Pirate's Booty", "Aged White Cheddar", "Glutten", "Free", "Trick"];
+
+console.log(longestWord(testArray));
+
