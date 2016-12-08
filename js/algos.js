b96c88f7e2ea34ef1ac6f7238a19a0bbc3c84cf9
kenobi82403 <kenobi82403@gmail.com>
2016-12-07 20:48:47 -0500

    pseudo code longest word problem

---
+++ js/algos.js
@@ -0,0 +1,15 @@
+// Release 0 : Find the Longest Phrase
+
+// In algos.js, write a function that takes an array of words or phrases and returns the longest word or phrase in the array. So if we gave your function the array of ["long phrase","longest phrase","longer phrase"], it would return "longest phrase". This is a logical reasoning challenge, so try to reason through this based on what you already know, rather than Googling the fanciest solution or looking up built-in sorting functions. Note that "some string".length will do what you expect in JS.
+// Algorithm
+
+// Function takes in Array of words/phrase
+//  - a variable to keep track of the index of the longest word with initial value of 0
+//  - iterate the whole Array
+//  - condition to see if the current word length of the index is larger than maxIndex
+//  - if so, then override with the current word length
+// Returns a string by referencing the array with the maxIndex variable
+
+
+
+// DRIVER CODE
