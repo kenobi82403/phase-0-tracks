a67b5a655e5c317924c7011486a69af0790eb01c
[KENDY NG] <kenobi82403@gmail.com>
2016-12-19 16:13:43 +0000

    pseudo code todo list program

--- databases/solo_project2/todo_list_program.rb
+++ databases/solo_project2/todo_list_program.rb
@@ -1,1 +1,54 @@
 # Write a Ruby program that uses persistent data to make your life better, or makes someone else's life better. Find several opportunities to try something not explicitly shown in the video. Can users create data? Retrieve it? Manipulate it? It's up to you. Push yourself, and decide with integrity when you are finished. Note that you do not need to use classes in this assignment -- keeping a class instance and a database row in sync is a lot of work, so that might be too much to tackle.
+
+# Require SQLite3 gem
+# Require faker gem
+
+# Create Class User List
+# Reader for array and name attribute
+
+# A method to set the name of the list owner.
+# Create an array empty array
+# Input: string - user name
+# Output: Boolean
+
+# A method to add item to list
+#   Input is a string
+#   Push item to list array
+#   call read method
+#   Output: Boolean
+
+# A method to read the list
+#   Prints the array on console
+
+# A method to delete item from list
+#   Input is an integer
+#   Declare temp array
+#   Loop through array and copy to new temp array
+#     Skip item if it matches integer ( each method)
+#   Save temp array to master list array
+#   Call read method
+#   Output: Boolean
+
+# A method to update item on list
+#   Input is an integer and a string
+#   Declare temp array
+#   Loop through list array and copy to new temp array
+#     If matches integer then update item (map bang!)
+#   Call read method
+#   Output: boolean
+
+# User Interface
+
+# Welcome to program
+#   Access an existing list or create a new list
+
+# Test CRUD
+
+# Allow user to create their own profile
+# Allow user to create a new list
+# Allow user to add item to list
+# Allow user to mark complete or incomplete
+# Allow user to delete item from list
+# Allow user to update item on list
+
+
---
+++ databases/solo_project2/todo_list_spec.rb
@@ -0,0 +1,10 @@
+# require relative main program file
+  
+# Allow user to create their own profile
+# Allow user to create a new list
+# Allow user to add item to list
+# Allow user to mark complete or incomplete
+# Allow user to delete item from list
+# Allow user to update item on list
+  
+  
