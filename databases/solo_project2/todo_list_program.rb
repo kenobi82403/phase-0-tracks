18aeede0e97ab2a32392cbde6429b06c5855e89e
kenobi82403 <kenobi82403@gmail.com>
2016-12-19 21:36:56 -0500

    extract methods from class and modified methods to work with databases

--- databases/solo_project2/todo_list_program.rb
+++ databases/solo_project2/todo_list_program.rb
@@ -11,7 +11,8 @@ owner_table = <<-SQL
   CREATE TABLE IF NOT EXISTS owners
   (
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
   );
 SQL
 
@@ -27,84 +28,80 @@ SQL
 db.execute(owner_table)
 db.execute(items_table)
 
-
-class TodoList
-  attr_reader :name, :list, :db
-  
-  def initialize(name, db)
-    @name = name
-    @list = db.execute("
-      SELECT item
-      FROM items
-      INNER JOIN owners
-      ON items.owner_id = owners.id;
-    ")
-    @db = db
-    create_owner(@name)
-    # p @list.class
-    true
-  end
+#     @pk = db.execute("
+#       SELECT id
+#       FROM owners
+#       WHERE name = ?;
+#     ", [@name])[0]
   
 # Add item to list
-  def add_item(item)
-    @list << item
-    print_list(@list)
-  end
+def add_item(db, item, pk)
+    db.execute("
+      INSERT INTO items
+      VALUES(?, ?);
+    ", [item, pk])
+end
 
 # Remove item from list
-  def remove_item(index_to_rm)
-    item_to_rm = @list[index_to_rm-1]
-    temp_list = []
-    @list.each do |item|
-      if item != item_to_rm
-        temp_list << item
-      end
-    end
-    @list = temp_list
-    print_list(@list)
-  end
+def remove_item(db, index_to_rm, pk)
+  list = retrieve_items(db, pk)
+  item_to_rm = list[index_to_rm-1]
+  db.execute("
+      DELETE FROM items
+      WHERE item = ?
+    ", [item_to_rm])  
+end
 
 # Update item on list
-  def update_item(index_to_update, new_item)
-    item_to_update = @list[index_to_update-1]
-    @list.map! do |item|
-      if item == item_to_update
-        item = new_item
-      else 
-        item
-      end
-    end
-    print_list(@list)
-  end  
+def update_item(db, index_to_update, new_item, pk)
+  list = retrieve_items(db, pk)
+  item_to_update = list[index_to_update-1]
+  db.execute("
+      UPDATE items
+      SET item = ?
+      WHERE item = ?
+    ", [new_item, item_to_update])  
+end  
 
 # Print List
-  def print_list(list)
-   puts "Current List:"
-   if(@list.length < 1) 
-     puts "Good job! You have no items on your list"
-   else 
-     for i in 0...@list.length
-       puts "#{i+1}: #{@list[i]}"
-     end
+def print_list(db, pk)
+ list = retrieve_items(db, pk)
+ puts "Current List:"
+ if(list.length < 1) 
+   puts "Good job! You have no items on your list"
+ else 
+   for i in 0...list.length
+     puts "#{i+1}: #{list[i]}"
    end
-  end
+ end
+end
 
-private
+def create_profile(name, password, db)
+  db.execute("
+      INSERT INTO owners (name, password)
+      VALUES (?, ?);
+    ", [name, password])
+end
 
-  def create_owner(name)
-    @db.execute("
-        INSERT INTO owners (name)
-        VALUES (?)
-      ", [name])
-  end
-  
-  def retrieve_items()
-    
-  end
-
+def retrieve_items(db, pk)
+  db.execute("
+    SELECT item
+    FROM items
+    WHERE owner_id = ?;
+  ", [pk])
 end
 
 # USER INTERFACE
+
+# create_profile("kendy", "password", db)
+# add_item(db, "take out trash", 1)
+# add_item(db, "mop the floor", 1)
+# add_item(db, "wash the dishes", 1)
+# remove_item(db, 2, 1)
+# update_item(db, 2, "buy milk", 1)
+# update_item(db, 4, "clean the house", 1)
+
+
 # puts "Welcome to my to do list 1.0!"
 # puts "What would you like to do?"
 # puts "N - new list"
@@ -120,4 +117,6 @@ end
 
 # p list
 
-list = TodoList.new('kendy', db);
+# list = TodoList.new('kendy', 'password', db);
+# list.add_item("wash dishes");
+# list.add_item("take trash out");
