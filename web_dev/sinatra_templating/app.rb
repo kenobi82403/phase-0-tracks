37ef2df1bcf26fcbd6825b3451bcff365bee5c14
kenobi82403 <kenobi82403@gmail.com>
2016-12-26 02:50:00 -0500

    added new sinatra template and routes

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,7 +1,9 @@
 # require gems
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
 
+# config to serve static resources in the static folder
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
@@ -20,8 +22,75 @@ end
 # create new students via
 # a form
 post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  sname = params['name']
+  campus = params['campus']
+  age = params['age'].to_i
+
+  db.execute("INSERT INTO students (name, campus, age) 
+              VALUES (?,?,?)", [sname, campus, age])
+
   redirect '/'
 end
 
 # add static resources
+
+# card route
+get '/cards' do
+  s_name = params[:name]
+  age = params[:age]
+  campus = params[:campus]
+  response = ""
+
+  if(s_name != nil)
+    query = "%#{s_name}%"
+    @students = db.execute("SELECT * FROM students WHERE name LIKE ?", [query])
+    puts "I'm in name #{query}"
+  elsif(age != nil)
+    query = "%#{age}%"
+    @students = db.execute("SELECT * FROM students WHERE age >= ?", [age.to_i])
+    puts "I'm in age #{query}"
+  elsif(campus != nil)    
+    query = "%#{campus}%"
+    @students = db.execute("SELECT * FROM students WHERE campus LIKE ?", [query])    
+    puts "I'm in campus #{query}"
+  else
+    @students = db.execute("SELECT * FROM students")
+  end
+
+  erb :cards
+end
+
+post '/cards' do 
+  s_name = params[:name] 
+  age = params[:age] 
+  campus = params[:campus] 
+  response = ''
+
+  if(s_name != '')
+    if(response == '')
+      response << '?'
+    end
+      response << "name=#{s_name}"
+  end 
+
+  if(age != '')
+    if(response == '')
+      response << '?'
+    else
+      response << '&'
+    end
+    response << "age=#{age}"
+  end
+
+  if(campus != '')
+    if(response == '')
+      response << '?'
+    else
+      response << '&'
+    end
+    response << "campus=#{campus}"
+  end
+  
+  puts response
+  redirect '/cards' + response
+end
