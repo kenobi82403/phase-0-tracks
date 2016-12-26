c6f2c9d158c5481ea6025abd4c48622bb28a9cb0
kenobi82403 <kenobi82403@gmail.com>
2016-12-25 21:38:51 -0500

    added 3 get routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require 'sinatra/reloader'
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -9,7 +10,7 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
+  "#{params[:name]} is #{params[:age]} years old....."
 end
 
 # write a GET route with
@@ -44,3 +45,29 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that display a make believe address
+
+get '/contact' do 
+  '1234 Times Square<br>New York, NY 10036'
+end
+
+# A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+get '/great_job' do
+  person_name = params[:name]
+  if(person_name)
+    "Good job, #{person_name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result. The data types are tricky here -- when will the data need to be (or arrive as) a string?
+get '/:num1/plus/:num2' do 
+  num1 = params[:num1]
+  num2 = params[:num2]
+  product = num1.to_i + num2.to_i
+  "#{num1} + #{num2} = #{product}"
+end
+
+# Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
